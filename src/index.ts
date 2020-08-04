c66bcd0c4647453ad0a7f91e5a21270cfe95a796
Sam <hatem@clearrain.xyz>
2020-08-04 19:01:06 -0400

    removed unnecessary functions and cleaned up the package

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,9 @@
-import { decodeJWT } from 'did-jwt'
-import * as Ajv from 'ajv'
-import * as constants from './constants'
-import { tagsSchema, createSchema } from './schemas'
+export const CONTRACT_SRC = 'eF0e3F3yV-r6jzNw2aH7ND4WgR7KrOU1uoUZAjVY1HI'
+export const CONTRACT_ID = 'iOJmwmkP-vQxEGKe0F4fWnoU2vBP4tfoilNZzYEPLQY'
 
-const { OPS } = constants
-const ajv = new Ajv({})
-
-interface UploadData {
-  jwt: string
-  tags: TxTags
+export const ROLES = {
+  OWNER: 'OWNER',
+  ADMIN: 'ADMIN',
+  MODERATOR: 'MODERATOR',
+  MEMBER: 'MEMBER'
 }
-
-interface TxTags {
-  'App-Name': string
-  'App-Version': string
-  Type: string
-  Did: string
-}
-
-interface OpData {
-  op: string
-  data: object
-  iat: number // timestamp
-  iss: string // issuer
-}
-
-export function isValidUpload (upload: UploadData): boolean {
-  if (!hasValidTags(upload)) return false
-
-  return isValidOp(upload.jwt)
-}
-
-export function isValidOp (jwt: string): boolean {
-  let message
-
-  try {
-    message = getJWTPayload(jwt)
-  } catch (e) {
-    return false
-  }
-
-  switch (message.op) {
-    case OPS.CREATE_COM:
-      return validateCreate(message)
-      break
-    default:
-      return false
-  }
-}
-
-function hasValidTags (tx: UploadData): boolean {
-  return ajv.validate(tagsSchema, tx.tags) as boolean
-}
-
-function validateCreate (createData: OpData): boolean {
-  return ajv.validate(createSchema, createData) as boolean
-}
-
-export function getJWTPayload (jwt: string): OpData {
-  return decodeJWT(jwt).payload as OpData
-}
-
-export { constants }
